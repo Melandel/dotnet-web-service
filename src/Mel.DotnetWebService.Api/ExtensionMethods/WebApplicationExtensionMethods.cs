e9cefb65b9a57b0b94a4f8ea38111aa911a69caf
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-06-12 21:00:15 +0200

    🏘 architecture    (default-url-redirection): redirect commonly used default urls to swaggerUI
    💌 Motivation: helpful feedback & allows `/` to not return http code `404`

--- src/Mel.DotnetWebService.Api/ExtensionMethods/WebApplicationExtensionMethods.cs
+++ src/Mel.DotnetWebService.Api/ExtensionMethods/WebApplicationExtensionMethods.cs
@@ -1,11 +1,34 @@
-﻿namespace Mel.DotnetWebService.Api.ExtensionMethods;
+﻿using Microsoft.AspNetCore.Rewrite;
+
+namespace Mel.DotnetWebService.Api.ExtensionMethods;
 
 public static class WebApplicationExtensionMethods
 {
+	const string SwaggerRoutePrefix = "swagger";
+	const string SwaggerRoute = $"{SwaggerRoutePrefix}/index.html";
 	public static WebApplication UseCustomSwaggerUI(this WebApplication app)
 	{
 		app.UseSwagger();
-		app.UseSwaggerUI(swaggerUIOptions => swaggerUIOptions.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.List));
+
+		app.UseSwaggerUI(swaggerUIOptions =>
+		{
+			swaggerUIOptions.RoutePrefix = SwaggerRoutePrefix;
+			swaggerUIOptions.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.List);
+		});
+
+		app.UseCustomRedirectionToSwaggerUiOnCommonlyUsedDefaultApiRoutes();
+
+		return app;
+	}
+
+	public static WebApplication UseCustomRedirectionToSwaggerUiOnCommonlyUsedDefaultApiRoutes(this WebApplication app)
+	{
+		var rewriteOptions = new RewriteOptions()
+			.AddRedirect("^$", SwaggerRoute)
+			.AddRedirect("^home.html$", SwaggerRoute)
+			.AddRedirect("^index.html$", SwaggerRoute);
+
+		app.UseRewriter(rewriteOptions);
 
 		return app;
 	}
--- src/Mel.DotnetWebService.Tests.FearlessProgramming/Usings.cs
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/Usings.cs
@@ -1,3 +1,4 @@
 ﻿global using NUnit.Framework;
 global using Mel.DotnetWebService.Tests.FearlessProgramming.ExtensionMethods;
+global using Mel.DotnetWebService.Tests.FearlessProgramming.TestData.TestCases;
 global using Mel.DotnetWebService.Tests.FearlessProgramming.TestEnvironments.TestServers;
