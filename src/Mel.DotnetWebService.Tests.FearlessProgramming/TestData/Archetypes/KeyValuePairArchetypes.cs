1f32e04e1b131f307e2b0d0dc2f365abcfa09bcf
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-12 08:48:17 +0200

    🏘 architecture    (enum-types): set up custom constraints for dotnet's `enum` types
    💌 Motivation: prevent common issues caused by dotnet's weak native constraints for `enum` types (cf [int/string permissibility](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) and the fact that the default value 0 is used whenever a (de)serialization operation fails)
    
    -fr-
    🏘 architecture    (types-enum): Mettre en place des contraintes pour les types `enum` de `dotnet`
    
    💌 Motivation: Empêcher des problèmes ultra-communs liés aux contraintes trop douces sur les types `enum` de `dotnet` (cf [permissibilité int/string](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) et l'impact d'avoir une valeur fonctionnelle définie en tant que 0 dans un contexte de désérialisation échouant à mi-chemin)

---
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/TestData/Archetypes/KeyValuePairArchetypes.cs
@@ -0,0 +1,9 @@
+﻿using Mel.DotnetWebService.CrossCuttingConcerns.EnumTypesHandling;
+
+namespace Mel.DotnetWebService.Tests.FearlessProgramming.TestData.Archetypes;
+
+public static class KeyValuePairArchetype
+{
+	public static readonly KeyValuePair<int, string> ZeroValuedEnumItem_Named_TechnicalDefaultEnumValue
+	= KeyValuePair.Create(0, EnumTypesNamingConvention.TechnicalDefaultEnumValue);
+}
