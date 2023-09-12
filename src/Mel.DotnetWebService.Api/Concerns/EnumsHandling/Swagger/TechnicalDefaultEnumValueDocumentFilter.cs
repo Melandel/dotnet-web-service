91038e31df954ebe2243da50af31c899fe89fc2a
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-12 08:48:17 +0200

    🏘 architecture    (enum-types): set up custom constraints for dotnet's `enum` types
    💌 Motivation: prevent common issues caused by dotnet's weak native constraints for `enum` types (cf [int/string permissibility](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) and the fact that the default value 0 is used whenever a (de)serialization operation fails)
    
    -fr-
    🏘 architecture    (types-enum): Mettre en place des contraintes pour les types `enum` de `dotnet`
    
    💌 Motivation: Empêcher des problèmes ultra-communs liés aux contraintes trop douces sur les types `enum` de `dotnet` (cf [permissibilité int/string](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) et l'impact d'avoir une valeur fonctionnelle définie en tant que 0 dans un contexte de désérialisation échouant à mi-chemin)

---
+++ src/Mel.DotnetWebService.Api/Concerns/EnumsHandling/Swagger/TechnicalDefaultEnumValueDocumentFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Mel.DotnetWebService.Api.Concerns.EnumsHandling.Swagger;
+
+class TechnicalDefaultEnumValueDocumentFilter : IDocumentFilter
+{
+	public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+	{
+		var schemasWithEnumType = swaggerDoc.Components.Schemas
+			.Select(schema => schema.Value)
+			.Where(enumDefinition => enumDefinition.Enum is [_, ..]);
+
+		foreach (var schemaWithEnumType in schemasWithEnumType)
+		{
+			var enumStringValuesExceptTechnicalDefaultEnumValue = schemaWithEnumType.Enum
+				.Where(iOpenApiAny => iOpenApiAny is not OpenApiString { Value: "TechnicalDefaultEnumValue" })
+				.ToList();
+
+			schemaWithEnumType.Enum = enumStringValuesExceptTechnicalDefaultEnumValue;
+		}
+	}
+}
