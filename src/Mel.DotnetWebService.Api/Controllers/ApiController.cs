3e31aa9f0ccf28474cd0aec45ad1da6679587fdf
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-12 08:48:17 +0200

    🏘 architecture    (enum-types): set up custom constraints for dotnet's `enum` types
    💌 Motivation: prevent common issues caused by dotnet's weak native constraints for `enum` types (cf [int/string permissibility](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) and the fact that the default value 0 is used whenever a (de)serialization operation fails)
    
    -fr-
    🏘 architecture    (types-enum): Mettre en place des contraintes pour les types `enum` de `dotnet`
    
    💌 Motivation: Empêcher des problèmes ultra-communs liés aux contraintes trop douces sur les types `enum` de `dotnet` (cf [permissibilité int/string](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) et l'impact d'avoir une valeur fonctionnelle définie en tant que 0 dans un contexte de désérialisation échouant à mi-chemin)

--- src/Mel.DotnetWebService.Api/Controllers/ApiController.cs
+++ src/Mel.DotnetWebService.Api/Controllers/ApiController.cs
@@ -4,6 +4,7 @@ namespace Mel.DotnetWebService.Api.Controllers;
 
 [ApiController]
 [Concerns.ErrorHandling.ActionFilters.StoreReceivingControllerContextInsideHttpContext]
+[Concerns.EnumsHandling.ActionFilters.ProhibitEnumsPassedAsIntegersInRouteOrQueryString]
 [Route("api/v{version:apiVersion}/[controller]/[action]")]
 public abstract class ApiController : ControllerBase
 {
