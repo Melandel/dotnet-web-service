91eff8d06a5d6c8761a04fd70d3f002f68c7c440
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-12 08:48:17 +0200

    🏘 architecture    (enum-types): set up custom constraints for dotnet's `enum` types
    💌 Motivation: prevent common issues caused by dotnet's weak native constraints for `enum` types (cf [int/string permissibility](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) and the fact that the default value 0 is used whenever a (de)serialization operation fails)
    
    -fr-
    🏘 architecture    (types-enum): Mettre en place des contraintes pour les types `enum` de `dotnet`
    
    💌 Motivation: Empêcher des problèmes ultra-communs liés aux contraintes trop douces sur les types `enum` de `dotnet` (cf [permissibilité int/string](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) et l'impact d'avoir une valeur fonctionnelle définie en tant que 0 dans un contexte de désérialisation échouant à mi-chemin)

--- src/Mel.DotnetWebService.Tests.FearlessProgramming/TestSuites/CodingStandards/EnumTypesMust.cs
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/TestSuites/CodingStandards/EnumTypesMust.cs
@@ -1,1 +1,137 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mel.DotnetWebService.Tests.FearlessProgramming.TestSuites.CodingStandards;
+
+class EnumTypesMust
+{
+	static readonly InMemoryTestServer TestServer;
+	static EnumTypesMust()
+	{
+		TestServer = InMemoryTestServer.Create();
+	}
+
+	[TestCaseSource(typeof(Types), nameof(Types.AllEnumTypesDefinedByOurOrganization))]
+	public void Define_ZeroValued_EnumItem_Named_TechnicalDefaultEnumValue(Type enumType)
+	{
+		// Arrange
+		Dictionary<int, string> enumTypeItems = Enum.GetValues(enumType)
+			.Cast<int>()
+			.ToDictionary(enumValue => enumValue, enumValue => Enum.GetName(enumType, enumValue));
+
+		// Act
+
+		// Assert
+		Assert.That(
+			enumTypeItems,
+			Contains.Item(KeyValuePairArchetype.ZeroValuedEnumItem_Named_TechnicalDefaultEnumValue));
+	}
+
+	[Test]
+	public async Task Be_Accepted_As_String_In_Request_Body()
+	{
+		// Arrange
+		var enumValueAsString = CityNameArchetype.SomeValue.ToString();
+		var payload = $@"{{ ""CityName"": ""{enumValueAsString}"" }}";
+		var requestUri = $"api/v3/stubbed-endpoints-specifically-created-for-tests/with-enum-in-output-field-and-in-request-body";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.PostAsync(
+			requestUri,
+			payload.ToJsonContent());
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+	}
+
+	[Test]
+	public async Task Be_Rejected_As_Int_In_Request_Body()
+	{
+		// Arrange
+		var enumValueAsInt = (int)CityNameArchetype.SomeValue;
+		var payload = $@"{{ ""CityName"": {enumValueAsInt} }}";
+		var requestUri = $"api/v3/stubbed-endpoints-specifically-created-for-tests/with-enum-in-output-field-and-in-request-body";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.PostAsync(
+			requestUri,
+			payload.ToJsonContent());
+		var response = await httpResponse.ToResponseObject<ValidationProblemDetails>();
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+		Assert.That(response, Is.Not.Null);
+		Assert.That(response.Errors, Does.ContainKey("$.CityName"));
+		Assert.That(response.Errors["$.CityName"], Is.Not.Empty);
+		Assert.That(
+			response.Errors["$.CityName"].First(),
+			Does.Contain($"The JSON value could not be converted to {typeof(ControllerTestDoubles.StubbedEndpointsSpecificallyCreatedForTests.RequestWithCityNameProperty).FullName}"));
+	}
+
+	[Test]
+	public async Task Be_Accepted_As_String_In_Request_QueryString()
+	{
+		// Arrange
+		var enumValueAsString = CityNameArchetype.SomeValue.ToString();
+		var requestUri = $"api/v3/stubbed-endpoints-specifically-created-for-tests/with-enum-in-output-field-and-in-query-string?cityName={enumValueAsString}";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.GetAsync(requestUri);
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+	}
+
+	[Test]
+	public async Task Be_Rejected_As_Int_In_Request_QueryString()
+	{
+		// Arrange
+		var enumValueAsInt = (int)CityNameArchetype.SomeValue;
+		var parameterName = "cityName";
+		var requestUri = $"api/v3/stubbed-endpoints-specifically-created-for-tests/with-enum-in-output-field-and-in-query-string?{parameterName}={enumValueAsInt}";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.GetAsync(requestUri);
+		var response = await httpResponse.ToResponseObject<ProblemDetails>();
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+		Assert.That(response.Extensions, Is.Not.Null);
+		Assert.That(response.Extensions, Does.ContainKey("IntegerValue").WithValue(enumValueAsInt));
+		Assert.That(response.Extensions, Does.ContainKey("EnumParameterName").WithValue(parameterName));
+		Assert.That(response.Extensions, Does.ContainKey("SupportedEnumValues"));
+		Assert.That(response.Extensions["SupportedEnumValues"], Is.EquivalentTo(new[] { "Toulouse", "Bordeaux", "Paris" }));
+	}
+
+	[Test]
+	public async Task Be_Accepted_As_String_In_Route()
+	{
+		// Arrange
+		var enumValueAsString = CityNameArchetype.SomeValue.ToString();
+		var requestUri = $"api/v3/stubbed-endpoints-specifically-created-for-tests/with-enum-in-output-field-and-in-route/{enumValueAsString}";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.GetAsync(requestUri);
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+	}
+
+	[Test]
+	public async Task Be_Rejected_As_Int_In_Route()
+	{
+		// Arrange
+		var enumValueAsInt = (int)CityNameArchetype.SomeValue;
+		var requestUri = $"api/v3/stubbed-endpoints-specifically-created-for-tests/with-enum-in-output-field-and-in-route/{enumValueAsInt}";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.GetAsync(requestUri);
+		var response = await httpResponse.ToResponseObject<ProblemDetails>();
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+		Assert.That(response.Extensions, Is.Not.Null);
+		Assert.That(response.Extensions, Does.ContainKey("IntegerValue"));
+		Assert.That(response.Extensions, Does.ContainKey("EnumParameterName"));
+		Assert.That(response.Extensions, Does.ContainKey("SupportedEnumValues"));
+	}
+}
