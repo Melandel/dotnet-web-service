5d98043a9189fdf440a3dc96a48ba3774b000305
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-12 08:48:17 +0200

    🏘 architecture    (enum-types): set up custom constraints for dotnet's `enum` types
    💌 Motivation: prevent common issues caused by dotnet's weak native constraints for `enum` types (cf [int/string permissibility](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) and the fact that the default value 0 is used whenever a (de)serialization operation fails)

--- src/Mel.DotnetWebService.Api/Program.cs
+++ src/Mel.DotnetWebService.Api/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCustomSerializationSettings();
 builder.Services.AddCustomControllers();
 builder.Services.AddCustomSwaggerGenerator();
 builder.Services.AddCustomExceptionHandlingCompliantWithRfc9457();
