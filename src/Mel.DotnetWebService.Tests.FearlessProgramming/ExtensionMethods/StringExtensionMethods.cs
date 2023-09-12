9118a13461df472e32c11f16bc8355134b22b67c
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-12 08:48:17 +0200

    🏘 architecture    (enum-types): set up custom constraints for dotnet's `enum` types
    💌 Motivation: prevent common issues caused by dotnet's weak native constraints for `enum` types (cf [int/string permissibility](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) and the fact that the default value 0 is used whenever a (de)serialization operation fails)
    
    -fr-
    🏘 architecture    (types-enum): Mettre en place des contraintes pour les types `enum` de `dotnet`
    
    💌 Motivation: Empêcher des problèmes ultra-communs liés aux contraintes trop douces sur les types `enum` de `dotnet` (cf [permissibilité int/string](https://stackoverflow.com/questions/49562774/what-is-the-best-way-to-prohibit-integer-value-for-enum-actions-parameter) et l'impact d'avoir une valeur fonctionnelle définie en tant que 0 dans un contexte de désérialisation échouant à mi-chemin)

---
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/ExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace Mel.DotnetWebService.Tests.FearlessProgramming.ExtensionMethods;
+
+public static class StringExtensionMethods
+{
+	public static HttpContent ToJsonContent(this string payloadAsString)
+	=> new StringContent(payloadAsString, Encoding.UTF8, "application/json");
+
+}
