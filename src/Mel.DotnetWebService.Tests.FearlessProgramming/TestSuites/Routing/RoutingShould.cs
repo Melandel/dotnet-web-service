11f30e13908542ecb013dde33ebc7fb15f63c31b
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-13 21:24:09 +0200

    🏘 architecture    (url-naming-convention): use `kebab-case` instead of the default `PascalCase`
    💌 Motivation: `-` hyphens are better word separators than `_` underscores and `UPPER CASE` letters because they are **not** a [word character](https://learn.microsoft.com/en-us/dotnet/standard/base-types/character-classes-in-regular-expressions?redirectedfrom=MSDN#WordCharacter)

---
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/TestSuites/Routing/RoutingShould.cs
@@ -0,0 +1,20 @@
+﻿namespace Mel.DotnetWebService.Tests.FearlessProgramming.TestSuites;
+
+class RoutingShould : TestSuiteUsingTestServer
+{
+	[Test]
+	public async Task Create_Urls_Using_KebabCase_AKA_Hyphen_Separated_Lower_Case_Words()
+	{
+		// Arrange
+		var requestUri = "stubbed-endpoints-specifically-created-for-tests/hello-world";
+
+		// Act
+		var httpResponse = await TestServer.HttpClient.GetAsync(requestUri);
+		var response = await httpResponse.GetContentAsString();
+
+		// Assert
+		Assert.That(httpResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+		Assert.That(response, Is.EqualTo("hello world"));
+	}
+}
+
