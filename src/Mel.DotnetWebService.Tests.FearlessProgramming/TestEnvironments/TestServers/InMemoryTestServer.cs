1a021f0ecfa9d125227b4dc3d85432a016ca1cd1
Minh-Tam TRAN (Shodo) <minhtam-tran@ext.groupe-cyrus.fr>
2025-02-20 12:06:30 +0100

    🏘 architecture    (error-handling): return `ProblemDetails` when exceptions are thrown
    💌 Motivation: keep a consistent structure organization-wise for error handling

--- src/Mel.DotnetWebService.Tests.FearlessProgramming/TestEnvironments/TestServers/InMemoryTestServer.cs
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/TestEnvironments/TestServers/InMemoryTestServer.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Mel.DotnetWebService.Api.Concerns.ErrorHandling;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Mel.DotnetWebService.Tests.FearlessProgramming.TestEnvironments.TestServers;
 
-internal class InMemoryTestServer : WebApplicationFactory<Program>
+class InMemoryTestServer : WebApplicationFactory<Program>
 {
 	public HttpClient HttpClient => CreateDefaultClient(DelegatingHandlerTestDouble.That_Writes_HttpResponse_In_TestOutput);
+	readonly HostEnvironment _hostEnvironment;
+	readonly Lazy<HttpProblemTypeArchetype.Deserializable[]> _httpProblemTypes;
+	InMemoryTestServer(HostEnvironment hostEnvironment)
+	{
+		_hostEnvironment = hostEnvironment;
+		_httpProblemTypes = new Lazy<HttpProblemTypeArchetype.Deserializable[]>(() =>
+		{
+			var httpProblemTypesAsHttpResponse = HttpClient.GetAsync("api/v1/http-problem-types")
+				.GetAwaiter()
+				.GetResult();
+			var httpProblemTypes = httpProblemTypesAsHttpResponse.ToResponseObject<HttpProblemTypeArchetype.Deserializable[]>()
+				.GetAwaiter()
+				.GetResult();
+			return httpProblemTypes;
+		});
+	}
 	protected override IHost CreateHost(IHostBuilder builder)
 	{
 		builder
+			.UseEnvironment(_hostEnvironment.ToString())
 			.ConfigureLogging(logging => logging.ClearProviders())
 			.ConfigureServices(services => services.AddController<ControllerTestDoubles.StubbedEndpointsSpecificallyCreatedForTests>());
+
 		return base.CreateHost(builder);
 	}
 
-	public static InMemoryTestServer Create()
-	=> new InMemoryTestServer();
+	public IReadOnlyCollection<string> HttpProblemTypeRoutes
+	=> _httpProblemTypes.Value
+		.Select(httpProblemType => httpProblemType.Uri.ToString())
+		.ToArray();
+
+	public HttpProblemType GetHttpProblemTypeByName(string httpProblemTypeName)
+	=> _httpProblemTypes.Value
+		.Single(httpProblemType => httpProblemType.Uri.ToString().EndsWith(httpProblemTypeName));
+
+	public static InMemoryTestServer Create(HostEnvironment hostEnvironment = HostEnvironment.Production)
+	=> new InMemoryTestServer(hostEnvironment);
 }
---
+++ src/Mel.DotnetWebService.Tests.FearlessProgramming/TestEnvironments/TestSpecificDataStructures/HostEnvironment.cs
@@ -0,0 +1,9 @@
+﻿namespace Mel.DotnetWebService.Tests.FearlessProgramming.TestEnvironments.TestSpecificDataStructures;
+
+enum HostEnvironment
+{
+	TechnicalDefaultEnumValue = 0,
+	Development = 1,
+	Staging = 2,
+	Production = 3,
+}
