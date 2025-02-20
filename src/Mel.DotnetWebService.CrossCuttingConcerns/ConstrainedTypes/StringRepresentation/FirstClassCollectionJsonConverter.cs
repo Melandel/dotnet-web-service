9ad50f739fb00bf53550d29664f46ca1b8b7a069
Minh-Tam TRAN (Shodo) <minhtam-tran@ext.groupe-cyrus.fr>
2025-02-20 12:09:23 +0100

    🏘 architecture    (error-detection): implement objects consistency constraints via exceptions thrown in constructors
    💌 Motivation: inconsistent objects should _not_ be allowed to be created. It should not be a thing, it should not happen.
    
    -fr-
    🏘 architecture    (détection-des-erreurs): Implémenter des contraintes de consistence dans les objets via des exceptions levées dans les constructeurs
    
    💌 Motivation: La création d'objets inconsistents devrait être _complètement_ impossible. Elle ne doit pas exister, elle ne doit pas être une possibilité envisageable.

---
+++ src/Mel.DotnetWebService.CrossCuttingConcerns/ConstrainedTypes/StringRepresentation/FirstClassCollectionJsonConverter.cs
@@ -0,0 +1,89 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Mel.DotnetWebService.CrossCuttingConcerns.ConstrainedTypes.StringRepresentation;
+
+public class FirstClassCollectionJsonConverter : JsonConverter<object>
+{
+	readonly static JsonConverter<object> DefaultConverter = (JsonConverter<object>)JsonSerializerOptions.Default.GetConverter(typeof(object));
+
+	public override bool CanConvert(Type typeToConvert)
+	=> typeToConvert.DefinesAFirstClassCollection();
+
+	public override object? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	=> DefaultConverter.Read(ref reader, typeToConvert, options);
+
+	public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+	{
+		value.GetType().DefinesConstructorWhoseParametersAllHaveAMatchingField(out var fields, out var properties);
+		writer.WriteRawValue(
+			JsonSerializer.Serialize(
+				(fields, properties) switch
+				{
+					([var field], []) => field.GetValue(value),
+						_ => BuildDictionary(value, fields, properties)
+				},
+				options));
+	}
+
+	Dictionary<string, object?> BuildDictionary(object value, FieldInfo[] fields, PropertyInfo[] properties)
+	{
+		var dictionary = new Dictionary<string, object?>();
+
+		foreach(var field in fields)
+		{
+			try
+			{
+				var fieldValue = field.GetValue(value);
+				if (fieldValue.IsReadOnlyCollectionWithoutBeingString(out var asReadOnlyCollection))
+				{
+					dictionary.Add($"{field.Name}_count", asReadOnlyCollection.Count);
+				}
+				else if (fieldValue.IsFirstClassCollectionWithKnownCount(out var count))
+				{
+					dictionary.Add($"{field.Name}_count", count);
+				}
+
+				dictionary.Add(field.Name, fieldValue);
+			}
+			catch
+			{
+			}
+		}
+
+		foreach(var property in properties)
+		{
+			if (property.PropertyType.IsEnumerableWithoutBeingString())
+			{
+				try
+				{
+					dictionary.Add(
+						property.Name,
+						property.GetValue(value) switch
+						{
+							null => null,
+							_ => "[..]"
+						});
+				}
+				catch (Exception e)
+				{
+					dictionary.Add(property.Name, e.GetType().FullName);
+				}
+			}
+			else
+			{
+				try
+				{
+					dictionary.Add(property.Name, property.GetValue(value));
+				}
+				catch (Exception e)
+				{
+					dictionary.Add(property.Name, e.GetType().FullName);
+				}
+			}
+		}
+
+		return dictionary;
+	}
+}
