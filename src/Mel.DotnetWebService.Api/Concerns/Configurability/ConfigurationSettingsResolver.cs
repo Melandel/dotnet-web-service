a819ec6fa003c38007241e4c7b6c22d67e230aa9
Minh-Tam TRAN <c_mtran@capsuletech.com>
2025-08-16 23:49:23 +0200

    🏘 architecture    (configuration-settings): hide all the configuration settings behind a configuration location ID accessed from appsettings.json
    💌 Motivation: make configuration settings resolution policy uniform and easily changeable organization-wide, by gathering its logic inside a single dedicated class
    
    -fr-
    🏘 architecture    (configuration): Cacher les paramètres de configuration derrière un identifiant d'emplacement de configuration, accessible depuis appsettings.json.
    
    💌 Motivation: Rendre la politique de résolution des paramètres de configuration uniforme et la rendre facilement modifiable à l'échelle de l'organisation, en regroupant sa logique dans une classe dédiée unique

---
+++ src/Mel.DotnetWebService.Api/Concerns/Configurability/ConfigurationSettingsResolver.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using Mel.DotnetWebService.CrossCuttingConcerns.Configurability;
+
+namespace Mel.DotnetWebService.Api.Concerns.Configurability;
+
+class ConfigurationSettingsResolver : IConfigurationSettingsResolver
+{
+	readonly IConfigurationBuilder _configurationBuilder;
+	readonly static JsonConfigurationFilter[] JsonConfigurationFilters = new[]
+	{
+		new JsonConfigurationFilter(Prefix: "Private", RemovePrefixFromConfigurationItemKey: true),
+		new JsonConfigurationFilter(Prefix: "Public", RemovePrefixFromConfigurationItemKey: false)
+	};
+	public ConfigurationSettingsResolver(IConfigurationBuilder configurationBuilder)
+	{
+		_configurationBuilder = configurationBuilder;
+	}
+
+	public void ResolveAllConfigurationSettings(string? configurationLocationMainIdentifier, string? configurationLocationFallbackIdentifier)
+	{
+		if (string.IsNullOrEmpty(configurationLocationMainIdentifier))
+		{
+			throw new ConfigurationErrorsException($"Could not find the following configuration setting: {nameof(configurationLocationMainIdentifier)}");
+		}
+
+		try
+		{
+			_configurationBuilder.AddJsonFile(
+				configurationLocationMainIdentifier,
+				JsonConfigurationFilters);
+		}
+		catch (Exception e)
+		{
+			if (string.IsNullOrEmpty(configurationLocationFallbackIdentifier))
+			{
+				throw new ConfigurationErrorsException($"Could not load the configuration located at {nameof(configurationLocationMainIdentifier)}", e);
+			}
+
+			try
+			{
+				_configurationBuilder.AddJsonFile(
+					configurationLocationMainIdentifier,
+					JsonConfigurationFilters);
+			}
+			catch (Exception ex)
+			{
+				throw new ConfigurationErrorsException($"Could not load neither the configuration located at {nameof(configurationLocationMainIdentifier)}, neither the configuration located at {nameof(configurationLocationFallbackIdentifier)}", ex);
+			}
+		}
+	}
+}
---
+++ src/Mel.DotnetWebService.Api/Concerns/Configurability/FilteredJsonConfigurationSource.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration.Json;
+
+namespace Mel.DotnetWebService.Api.Concerns.Configurability;
+
+class FilteredJsonConfigurationSource : JsonConfigurationSource
+{
+	public IReadOnlyCollection<JsonConfigurationFilter> Filters { get; set; } = Array.Empty<JsonConfigurationFilter>();
+	public override IConfigurationProvider Build(IConfigurationBuilder builder)
+	{
+		EnsureDefaults(builder);
+
+		if (!Filters.Any())
+		{
+			return new JsonConfigurationProvider(this);
+		}
+
+		return new FilteredJsonConfigurationProvider(this, Filters);
+	}
+}
